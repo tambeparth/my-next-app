d72f6d32aa48712e29fadf691fbdc36d6af38ab6
partht_12 <tambeparth27@gmail.com>
2025-06-20 18:07:57 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -18,7 +18,6 @@ const nextConfig = {
   },
   // Suppress hydration warnings in development
   reactStrictMode: true,
-  // Added configuration for line ending test
   onDemandEntries: {
     // period (in ms) where the server will keep pages in the buffer
     maxInactiveAge: 25 * 1000,
